40d3dfee5d7f284e94da789515c04ab77beea2e2
nikitrifonov <235618>
2017-03-22 22:20:57 +0200

    update

--- Test1/SnakeMatrix/Program.cs
+++ Test1/SnakeMatrix/Program.cs
@@ -16,14 +16,51 @@ namespace SnakeMatrix
             int rows = int.Parse(values[0]);
             int cols = int.Parse(values[1]);
             int startNumber = int.Parse(values[2]);
-            int[,] matrix = new int[rows, cols];
+            int?[,] matrix = new int?[rows, cols];
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            int totalSteps = rows * cols;
+            int positionY = 0;
+            int positionX = 0;
+            Direction direction = Direction.Right;
+            for (int i = 1; i <= totalSteps; i++)
             {
-                for (int f = 0; f < matrix.GetLength(1); f++)
+                if (direction == Direction.Right && (positionX >= cols - 1 || matrix[positionY, positionX + 1] != null))
                 {
-                    matrix[i, f] += startNumber;
-                    startNumber++;
+                    direction = Direction.Down;
+                    
+                }
+                if (direction == Direction.Down && (positionY >= rows - 1 || matrix[positionY + 1, positionX] != null))
+                {
+                    direction = Direction.Left;
+                    
+                }
+                if (direction == Direction.Left && (positionX <= 0 || matrix[positionY, positionX - 1] != null))
+                {
+                    direction = Direction.Up;
+                    
+                }
+                if (direction == Direction.Up &&  matrix[positionY - 1, positionX] != null)
+                {
+                    direction = Direction.Right;
+                    
+                }
+                matrix[positionY, positionX] = startNumber;
+                startNumber++;
+
+                switch (direction)
+                {
+                    case Direction.Right:
+                        positionX++;
+                        break;
+                    case Direction.Down:
+                        positionY++;
+                        break;
+                    case Direction.Left:
+                        positionX--;
+                        break;
+                    case Direction.Up:
+                        positionY--;
+                        break;
                 }
             }
             Utility.PrintMatrix(matrix);
