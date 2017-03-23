e51dbd5fb08334e10d8821a9b85cb53711c7cd2c
nikitrifonov <235618>
2017-03-23 22:07:38 +0200

    hitting wall and 2nd snake matrix

---
+++ Test1/HittingWalls/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HittingWalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter matrix dimensions:");
+            string matrixInput = Console.ReadLine();
+            Console.WriteLine("Enter initial player position:");
+            string positionInput = Console.ReadLine();
+            Console.WriteLine("Enter player directions:");
+            string directionInput = Console.ReadLine().ToUpper();
+            int directionsCount = directionInput.Length;
+
+            string[] input = matrixInput.Split(',');
+            string[] position = positionInput.Split(',');
+
+            int playerRowPosition = int.Parse(position[0]);
+            int playerColPosition = int.Parse(position[1]);
+
+            int row = int.Parse(input[0]);
+            int col = int.Parse(input[1]);
+            int[,] matrix = new int[row, col];
+
+            int numberOfColisions = 0;
+            for (int i = 0; i < directionsCount; i++)
+            {
+                char direction = ReadDirection(directionInput, i);
+
+                if (direction == 'R')
+                {
+                    if (playerColPosition + 1 <= col - 1)
+                    {
+                        playerColPosition++;
+                    }
+                    else
+                    {
+                        numberOfColisions++;
+                        Console.WriteLine("You hit a wall at position {0},{1} this is your {2} time", playerRowPosition, playerColPosition, numberOfColisions);
+                    }
+                }
+                if (direction == 'L')
+                {
+                    if (playerColPosition - 1 >= 0)
+                    {
+                        playerColPosition--;
+                    }
+                    else
+                    {
+                        numberOfColisions++;
+                        Console.WriteLine("You hit a wall at position {0},{1} this is your {2} time", playerRowPosition, playerColPosition, numberOfColisions);
+                    }
+                }
+                if (direction == 'D')
+                {
+                    if (playerRowPosition + 1 <= row -1)
+                    {
+                        playerRowPosition++;
+                    }
+                    else
+                    {
+                        numberOfColisions++;
+                        Console.WriteLine("You hit a wall at position {0},{1} this is your {2} time", playerRowPosition, playerColPosition, numberOfColisions);
+                    }
+                }
+                if (direction == 'U')
+                {
+                    if (playerRowPosition - 1 >= 0)
+                    {
+                        playerRowPosition--;
+                    }
+                    else
+                    {
+                        numberOfColisions++;
+                        Console.WriteLine("You hit a wall at position {0},{1} this is your {2} time", playerRowPosition, playerColPosition, numberOfColisions);
+                    }
+                }
+            }
+        }
+
+        private static char ReadDirection(string directions, int directionCount)
+        {
+            
+            return directions.ElementAt(directionCount);
+            
+        }
+    }
+}
---
+++ Test1/SnakeMatrix2ndEdition/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace SnakeMatrix2ndEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] values = input.Split(',');
+            int n = int.Parse(values[0]);
+            int m = int.Parse(values[1]);
+            int startNumber = int.Parse(values[2]);
+            int?[,] matrix = new int?[n, m];
+
+            int row = 0;
+            int col = 0;
+            int steps = n * m;
+            Directions direction = Directions.Right;
+
+            for (int i = 1; i <= steps; i++)
+            {
+                if (direction == Directions.Right && (col >= m - 1|| matrix[row,col + 1] != null))
+                {
+                    direction = Directions.Down;
+                }
+                if (direction == Directions.Down && (row  >= n - 1 || matrix[row + 1, col] != null))
+                {
+                    direction = Directions.Left;
+                }
+                if (direction == Directions.Left && (col <= 0 || matrix[row, col - 1] != null))
+                {
+                    direction = Directions.Up;
+                }
+                if (direction == Directions.Up &&  matrix[row - 1, col] != null)
+                {
+                    direction = Directions.Right;
+                }
+
+                matrix[row, col] = startNumber;
+                startNumber++;
+
+                switch (direction)
+                {
+                    case Directions.Right:
+                        col++;
+                        break;
+                    case Directions.Left:
+                        col--;
+                        break;
+                    case Directions.Down:
+                        row++;
+                        break;
+                    case Directions.Up:
+                        row--;
+                        break;
+                }
+            }
+            Utility.PrintMatrix(matrix);
+        }
+    }
+}
