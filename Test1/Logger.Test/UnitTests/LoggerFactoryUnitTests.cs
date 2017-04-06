fd330a5cd650734163439ca6319d6e0594f5c150
nikitrifonov <235618>
2017-04-06 17:26:04 +0300

    #6 Logger - Create unit tests for LoggerFactory

--- Test1/Logger.Test/UnitTests/LoggerFactoryUnitTests.cs
+++ Test1/Logger.Test/UnitTests/LoggerFactoryUnitTests.cs
@@ -12,14 +12,6 @@ namespace Logger.Test.UnitTests
     public class LoggerFactoryUnitTests
     {
         [TestMethod]
-        public void GetLoggerWithoutConfigurationReturnsFileLogger()
-        {
-            var logger = LoggerFactory.GetLogger();
-
-            Assert.IsInstanceOfType(logger, typeof(FileLogger));
-        }
-
-        [TestMethod]
         public void GetLoggerWithConfigurationReturnsConsoleLogger()
         {
             LoggerFactory.Configuration.DefaultLoggerType = LoggerType.ConsoleLogger;
@@ -29,33 +21,66 @@ namespace Logger.Test.UnitTests
         }
 
         [TestMethod]
-        public void SerializeSerializesCorrectly()
-        {
-            //Arrange
-            var serializer = new Common.JsonSerializer();
-            var input = new FileLoggerOptions()
-            {
-                FileName = "Logthis",
-                FilePath = "../../"
-            };
-
-            //Act
-            var result = serializer.Serialize(input);
-            FileLoggerOptions deserializedResult = Newtonsoft.Json.JsonConvert.DeserializeObject<FileLoggerOptions>(result);
-
-            //Assert
-            Assert.AreEqual(input.File, deserializedResult.File);
-            Assert.AreEqual(input.FileName, deserializedResult.FileName);
-            Assert.AreEqual(input.FilePath, deserializedResult.FilePath);
-        }
-
-        [TestMethod]
         public void GetLoggerWithConfigurationReturnsFilelogger()
         {
             LoggerFactory.Configuration.DefaultLoggerType = LoggerType.FileLogger;
             var logger = LoggerFactory.GetLogger();
 
             Assert.IsInstanceOfType(logger, typeof(FileLogger));
+        }
+
+        [ExpectedException(typeof(NotSupportedException))]
+        [TestMethod]
+        public void GetLoggerWithLoggerTypeThatDoesntExist()
+        {
+            //Arrange
+            LoggerFactory.Configuration.DefaultLoggerType = (LoggerType)3;
+
+            //Act
+            var logger = LoggerFactory.GetLogger();
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void GetLoggerReturnsFilelogger()
+        {
+            var logger = LoggerFactory.GetLogger(LoggerType.FileLogger);
+
+            Assert.IsInstanceOfType(logger, typeof(FileLogger));
+        }
+
+        [TestMethod]
+        public void GetLoggerReturnsConsoleLogger()
+        {
+            var logger = LoggerFactory.GetLogger(LoggerType.ConsoleLogger);
+
+            Assert.IsInstanceOfType(logger, typeof(ConsoleLogger));
+        }
+
+        [TestMethod]
+        public void GetLoggerCachesLoggers()
+        {
+            var fileLogger = LoggerFactory.GetLogger(LoggerType.FileLogger);
+            var consoleLogger = LoggerFactory.GetLogger(LoggerType.ConsoleLogger);
+            var fileLogger1 = LoggerFactory.GetLogger(LoggerType.FileLogger);
+            var consoleLogger1 = LoggerFactory.GetLogger(LoggerType.ConsoleLogger);
+
+            Assert.AreEqual(fileLogger, fileLogger1);
+            Assert.AreEqual(consoleLogger, consoleLogger1);
+        }
+   
+        [TestMethod]
+        public void ClearLoggersClearsTheCache()
+        {
+            var fileLogger = LoggerFactory.GetLogger(LoggerType.FileLogger);
+            var consoleLogger = LoggerFactory.GetLogger(LoggerType.ConsoleLogger);
+            LoggerFactory.ClearLoggers();
+            var fileLogger1 = LoggerFactory.GetLogger(LoggerType.FileLogger);
+            var consoleLogger1 = LoggerFactory.GetLogger(LoggerType.ConsoleLogger);
+            
+            Assert.AreNotEqual(fileLogger, fileLogger1);
+            Assert.AreNotEqual(consoleLogger, consoleLogger1);
         }
 
         [TestCleanup]
