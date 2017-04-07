469dd722544cc45c08a938e3635946e5c5a749be
nikitrifonov <235618>
2017-04-07 15:33:12 +0300

    #9 Rename the projects Test1 and Test2 to something that makes sense

---
+++ LearningProjects/Logger.Test/UnitTests/FileLoggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logger.Loggers;
+using Logger.Configuration;
+using Moq;
+
+namespace Logger.Test.UnitTests
+{
+    [TestClass]
+    public class FileLoggerTests
+    {
+        [TestMethod]
+        public void CreateFileLoggerWithDefaultSettingsReturnsFileLogger()
+        {
+            //Arrange
+           
+            //Act
+
+            //Assert
+          
+        }
+    }
+}
