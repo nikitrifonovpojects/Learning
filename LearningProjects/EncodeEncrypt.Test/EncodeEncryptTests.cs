3950a47e7f765108e68b578266d531faafbaa662
nikitrifonov <235618>
2017-04-21 15:25:55 +0300

    #11 Encode and encrypt

---
+++ LearningProjects/EncodeEncrypt.Test/EncodeEncryptTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EncodeEncrypt;
+
+namespace EncodeEncrypt.Test
+{
+    [TestClass]
+    public class EncodeEncryptTests
+    {
+        [TestMethod]
+        public void TestWithFirstExample()
+        {
+            //Arrange
+            string message = "TELERIKACADEMY";
+            string cypher = "SOFTWARE";
+            var expected = @"BKOXHI\EQOGX[YSOFTWARE8";
+            //Act
+            var textCypher = Program.Encrypt(message, cypher) + cypher;
+            var result = Program.Encode(textCypher) + cypher.Length;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestWithSecondExample()
+        {
+            //Arrange
+            string message = "AAABB";
+            string cypher = "BBBBBBA";
+            var expected = "ABBAA6BA7";
+            //Act
+            var textCypher = Program.Encrypt(message, cypher) + cypher;
+            var result = Program.Encode(textCypher) + cypher.Length;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestWithThirdExample()
+        {
+            //Arrange
+            string message = "JOHNY";
+            string cypher = "DEPPP";
+            var expected = "KKICXDE3P5";
+            //Act
+            var textCypher = Program.Encrypt(message, cypher) + cypher;
+            var result = Program.Encode(textCypher) + cypher.Length;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LearningProjects/EncodeEncrypt/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeEncrypt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string cypher = Console.ReadLine();
+
+            var textCypher = Encrypt(message, cypher) + cypher;
+            var compressed = Encode(textCypher) + cypher.Length;
+
+            Console.WriteLine(compressed);
+        }
+
+        public static string Encode(string message)
+        {
+            var encodedText = new StringBuilder(message.Length);
+            int indexMessage = 0;
+            while (indexMessage < message.Length)
+            {
+                char currentSymbol = message[indexMessage];
+                int scanIndex = indexMessage + 1;
+                int repeatLength = 1;
+                while (scanIndex < message.Length && message[scanIndex] == currentSymbol)
+                {
+                    repeatLength++;
+                    scanIndex++;
+                }
+
+                indexMessage = scanIndex;
+                if (repeatLength > 2)
+                {
+                    encodedText.Append(repeatLength);
+                    encodedText.Append(currentSymbol);
+                }
+                else
+                {
+                    encodedText.Append(new string(currentSymbol, repeatLength));
+                }
+            }
+
+            return encodedText.ToString();
+        }
+
+        public static string Encrypt(string message, string cypher)
+        {
+            var cypherText = new StringBuilder(message);
+            var maxlenght = Math.Max(message.Length, cypher.Length);
+
+            for (int i = 0; i < maxlenght; i++)
+            {
+                var messageIndex = i % message.Length;
+                var cypherIndex = i % cypher.Length;
+                var charInMessage = cypherText[messageIndex] - 'A';
+                var charInCypher = cypher[cypherIndex] - 'A';
+
+                cypherText[messageIndex] = (char)('A' + (charInMessage ^ charInCypher));
+            }
+
+            return cypherText.ToString();
+        }
+    }
+}
