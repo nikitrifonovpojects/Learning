f33c3699d12e40d3f013bde45003717cf585f364
nikitrifonov <235618>
2017-04-21 20:14:21 +0300

    #12 Add and subtract

---
+++ LearningProjects/AddAndSubtract.Test/AddSubtractCalculatorUnitTest.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace AddAndSubtract.Test
+{
+    [TestClass]
+    public class AddSubtractCalculatorUnitTest
+    {
+        [TestMethod]
+        public void CalculateSubtractWithDecimalNumbers()
+        {
+            //Arrange
+            string input = "100.256-50.256=";
+            decimal expected = 50;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateAddWithDecimalNumbers()
+        {
+            //Arrange
+            string input = "100.256+500.256=";
+            decimal expected = 600.512m;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateAddWithNegativeNumbers()
+        {
+            //Arrange
+            string input = "-100+-500=";
+            decimal expected = -600;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateSubtractWithPositiveNumbers()
+        {
+            //Arrange
+            string input = "100-50=";
+            decimal expected = 50;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateAddWithPositiveNumbers()
+        {
+            //Arrange
+            string input = "100+500=";
+            decimal expected = 600;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithNegativeAndPositiveNumbers()
+        {
+            //Arrange
+            string input = "-250+100+500+-60=";
+            decimal expected = 290;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithZero()
+        {
+            //Arrange
+            string input = "-0+0.00001=";
+            decimal expected = 0.00001m;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithPositiveNumbersOnly()
+        {
+            //Arrange
+            string input = "1010+126+4589765.256-2563.248+365.47-0.57+1478.524785=";
+            decimal expected = 4590181.432785m;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithNegativeNumbersOnly()
+        {
+            //Arrange
+            string input = "-1010+-126+-4589765.256--2563.248+-365.47--0.57+-1478.524785=";
+            decimal expected = -4590181.432785m;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithPositiveNumbersAndZero()
+        {
+            //Arrange
+            string input = "125+0-25+200-0=";
+            decimal expected = 300;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithNegativeNumbersAndZero()
+        {
+            //Arrange
+            string input = "-125+-0--25+-200-0=";
+            decimal expected = -300;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void CalculateWithMoreThanTwoOperatorsInARow()
+        {
+            //Arrange
+            string input = "-125++-0+--25-+-200---50=";
+            decimal expected = 50;
+
+            //Act
+            var calculator = new AddSubtractCalculator();
+            decimal result = calculator.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ LearningProjects/AddAndSubtract/AddSubtractCalculator.cs
@@ -0,0 +1,65 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddAndSubtract
+{
+    public class AddSubtractCalculator : AbstractCalculator
+    {
+        public override bool CheckForMoreThanOneOperator(string currentOperator, string nextOperator)
+        {
+            if (currentOperator != string.Empty)
+            {
+                if (nextOperator == "+" || nextOperator == "-")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override decimal PerformCalculation(decimal result, string currentOperator, decimal number)
+        {
+            switch (currentOperator)
+            {
+                case "+":
+                    result += number;
+                    break;
+                case "-":
+                    result -= number;
+                    break;
+                default:
+                    Console.WriteLine("Error in PerformCalculation");
+                    break;
+            }
+
+            return result;
+        }
+
+        public override string PrioritizeOperator(string currentOperator, string nextOperator)
+        {
+            if (currentOperator == "+" && nextOperator == "+")
+            {
+                currentOperator = "+";
+            }
+            else if (currentOperator == "+" && nextOperator == "-")
+            {
+                currentOperator = "-";
+            }
+            else if (currentOperator == "-" && nextOperator == "-")
+            {
+                currentOperator = "+";
+            }
+            else if (currentOperator == "-" && nextOperator == "+")
+            {
+                currentOperator = "-";
+            }
+
+            return currentOperator;
+        }
+    }
+}
