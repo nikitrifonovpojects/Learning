407f204bd03746ac52774ca6b130fbef6f4b0a97
nikitrifonov <235618>
2017-04-21 15:26:41 +0300

    #1 Finish the Diagonal Matrix Movement task

---
+++ LearningProjects/DiagonalMatrixMovement/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalMatrixMovement
+{
+    public class Obstacle
+    {
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+    }
+}
