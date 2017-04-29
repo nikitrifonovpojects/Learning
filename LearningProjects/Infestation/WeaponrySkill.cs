e352f199894edf72ef5d5ec32eb66291007a2805
nikitrifonovprojects <do_not_stop@abv.bg>
2017-04-29 15:31:25 +0300

    #15 - Removing commented code Infestation project working

--- LearningProjects/Infestation/WeaponrySkill.cs
+++ LearningProjects/Infestation/WeaponrySkill.cs
@@ -5,14 +5,8 @@ using System.Text;
 
 namespace Infestation
 {
-    public class WeaponrySkill : AbstractSupplement //Weapon
+    public class WeaponrySkill : AbstractSupplement 
     {
-        //public override void ReactTo(ISupplement otherSupplement)
-        //{
-        //    if (otherSupplement is Weapon)
-        //    {
-        //        this.foundInteraction = true;
-        //    }
-        //}
+        
     }
 }
