906d825852aa5210faf26770c441b4d5846b08fc
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-01 18:03:54 +0300

    #19 - passed all tests Army of Creatures

---
+++ LearningProjects/ArmyOfCreatures/Extended/Specialties/DoubleAttackWhenAttacking.cs
@@ -0,0 +1,47 @@
+﻿namespace ArmyOfCreatures.Extended.Specialties
+{
+    using System;
+    using System.Globalization;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Specialties;
+
+    public class DoubleAttackWhenAttacking : Specialty
+    {
+        private int rounds;
+        public DoubleAttackWhenAttacking(int rounds)
+        {
+            if (rounds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("rounds", "The number of rounds should be greater than 0");
+            }
+
+            this.rounds = rounds;
+        }
+
+        public override void ApplyWhenAttacking(ICreaturesInBattle attackerWithSpecialty, ICreaturesInBattle defender)
+        {
+            if (attackerWithSpecialty == null)
+            {
+                throw new ArgumentNullException("defenderWithSpecialty");
+            }
+
+            if (defender == null)
+            {
+                throw new ArgumentNullException("attacker");
+            }
+
+            if (this.rounds <= 0)
+            {
+                return;
+            }
+
+            attackerWithSpecialty.CurrentAttack *= 2;
+            this.rounds--;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}({1})", base.ToString(), this.rounds);
+        }
+    }
+}
