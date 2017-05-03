50591e97b56ed9dd73decfa138babd6f134a1ac7
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-04 00:21:24 +0300

    #21 - Yes it is done go away now , is was a stupid exception message Fast And Furious

--- LearningProjects/FastAndFurious.ConsoleApplication/Models/Common/Constants/ModelsConstants.cs
+++ LearningProjects/FastAndFurious.ConsoleApplication/Models/Common/Constants/ModelsConstants.cs
@@ -3,7 +3,7 @@
     public class ModelsConstants
     {
         public const string ObjectCreationSuccess = "{0} - successfully created!";
-        public const string DriverCannotBeAssignedToTheSameRaceTrackMoreThanOnce = "Cannot sign the same driver for a single race more than once!";
+        public const string DriverCannotBeAssignedToTheSameRaceTrackMoreThanOnce = "Cannot sign the same participant to a track";
         public const string DriverCannotBeAssignedTheTrackHasMaximumParticipants = "Cannot sign the driver because the race track is full!";
         public const string NoRacesYetMessage = "No races yet for {0}";
     }
