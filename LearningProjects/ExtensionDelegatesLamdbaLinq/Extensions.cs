aa80b9559b4ff41540bc5525e3832f228c06ade0
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-08 19:57:14 +0300

    Adding part of the ExtensionDelegatesLamdbaLinq homework

---
+++ LearningProjects/ExtensionDelegatesLamdbaLinq/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionDelegatesLamdbaLinq
+{
+    public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder builder, int index, int lenght)
+        {
+            var result = new StringBuilder();
+            var substring = builder.ToString().Substring(index, lenght);
+            result.Append(substring);
+
+            return result;
+        }
+
+        public static IEnumerable<T> Sum<T>(this IEnumerable<T> input)
+        {
+            var result = (from x in input select x).Sum<T>();
+            
+
+            return result;
+        }
+
+        public static IEnumerable<T> Product<T>(this IEnumerable<T> input)
+        {
+            var result = (from x in input select x).Product<T>();
+
+
+            return result;
+        }
+
+        public static IEnumerable<T> Min<T>(this IEnumerable<T> input)
+        {
+            var result = (from x in input select x).Min<T>();
+
+
+            return result;
+        }
+
+        public static IEnumerable<T> Max<T>(this IEnumerable<T> input)
+        {
+            var result = (from x in input select x).Max<T>();
+
+
+            return result;
+        }
+
+        public static IEnumerable<T> Average<T>(this IEnumerable<T> input)
+        {
+            var result = (from x in input select x).Average<T>();
+
+
+            return result;
+        }
+
+        //public static Array FirstBeforeLastName( students)
+        //{
+        //    var result = (from student in students select student).
+                         
+                         
+        //}
+    }
+}
---
+++ LearningProjects/ExtensionDelegatesLamdbaLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionDelegatesLamdbaLinq
+{
+    //1. Implement an extension method Substring(int index, int length) for the class StringBuilder that returns new StringBuilder and has the same functionality as Substring in the class String.
+    //2. Implement a set of extension methods for IEnumerable<T> that implement the following group functions: sum, product, min, max, average.
+    //3.Write a method that from a given array of students finds all students whose first name is before its last name alphabetically.
+
+    public class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
