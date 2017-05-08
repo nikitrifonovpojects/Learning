c93bd7e0292073c41da2bc335ea07855413fdfd9
nikitrifonovprojects <do_not_stop@abv.bg>
2017-05-08 14:15:40 +0300

    #24 tests Logger - Create unit tests for all of the formatters

---
+++ LearningProjects/Logger.Test/IntergrationTests/DateTimeFormatterTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Logger.Common.Enum;
+using Logger.Common.Formatters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Test.IntergrationTests
+{
+    [TestClass]
+    public class DateTimeFormatterTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            File.Delete(@"..\Log.txt");
+        }
+
+        [TestMethod]
+        public void CreateDefaultFileLoggerWithDateTimeFormatter()
+        {
+            //Arrange
+            LoggerFactory.Configuration.DefaultLoggerType = LoggerType.FileLogger;
+            LoggerFactory.Configuration.Formatter = new DateTimeFormatter();
+            string inputLog = "Logs";
+            string timeOfLog = DateTime.Now.ToString();
+            string expected = string.Join("-", timeOfLog, inputLog + Environment.NewLine);
+
+            //Act
+            var testFileLogger = LoggerFactory.GetLogger();
+            testFileLogger.Log(inputLog);
+
+            //Assert
+            string result = File.ReadAllText(@"..\Log.txt");
+            Assert.AreEqual(expected, result);
+            File.Delete(@"..\Log.txt");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            LoggerFactory.ClearLoggers();
+        }
+    }
+}
---
+++ LearningProjects/Logger.Test/IntergrationTests/LeapYearFormatterTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Logger.Common.Enum;
+using Logger.Common.Formatters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Test.IntergrationTests
+{
+    [TestClass]
+    public class LeapYearFormatterTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            File.Delete(@"..\Log.txt");
+        }
+
+        [TestMethod]
+        public void CreateDefaultFileLoggerWithLeapYearFormatter()
+        {
+            //Arrange
+            LoggerFactory.Configuration.DefaultLoggerType = LoggerType.FileLogger;
+            LoggerFactory.Configuration.Formatter = new LeapYearFormatter();
+            string inputLog = "Logs";
+            string timeOfLog = DateTime.Now.ToString("M/dd/yyyy hh:mm:ss");
+            string leapYear = "is not a leapyear";
+            string expected = string.Format("Log - {0} : {1} - {2}", timeOfLog, leapYear, inputLog + Environment.NewLine);
+
+            //Act
+            var testFileLogger = LoggerFactory.GetLogger();
+            testFileLogger.Log(inputLog);
+
+            //Assert
+            string result = File.ReadAllText(@"..\Log.txt");
+            Assert.AreEqual(expected, result);
+            File.Delete(@"..\Log.txt");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            LoggerFactory.ClearLoggers();
+        }
+    }
+}
